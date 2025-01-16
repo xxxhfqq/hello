1ff3ba35f35369cf4fb50b846fadb15a61e6fa4f
xxxhf <2731897659@qq.com>
2025-01-17 00:39:39 +0800

    1

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+#include<stdio.h>
+#include<vector>
+#include<cctype>
+using std::vector;
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::to_string;
+typedef struct {
+    int x;
+    int y;
+} Point;
+int main(int argc, char const *argv[]) {
+    int i;
+    i = 0;
+    return 0;
+}
